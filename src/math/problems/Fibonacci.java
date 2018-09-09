e738f62e8532dc3a916c6f49785d8367ba1a4647
mnihussain <md.nasiribnehussain@gmail.com>
2018-09-09 16:47:20 -0400

    Fibonacci Series Done

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -5,7 +5,16 @@ public class Fibonacci {
          /*
           Write 40 Fibonacci numbers with java.
          */
-
+         int counter = 0;
+         int next = 1;
+         int prev = 0;
+         for (int i = 0; i <40; i++){
+             System.out.println(next);
+             next = next + prev;
+             prev = next - prev;
+             counter++;
+         }
+        System.out.println("Total Fibonacci numbers are = " + counter);
 
     }
 }
