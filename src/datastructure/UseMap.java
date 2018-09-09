9ea693e0774df93f67e9b242b7a616ab6635af33
mnihussain <md.nasiribnehussain@gmail.com>
2018-09-09 16:34:54 -0400

    Factorial Done

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -7,7 +7,7 @@ public class UseMap {
 		 * Demonstrate how to use Map that includes storing and retrieving elements.
 		 * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 *
+		 * For each method
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
 
--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,6 +11,21 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
-
+        int a = 5;
+        int b = 1;
+     do { b *= a;
+     a--;
+        } while (a>0);
+        System.out.println("Factorial by Iteration of 5! = " + b);//Iteration
+    int d= Recursion(5);
+        System.out.println("Factorial by Recursion of 5! = " + d);//Recursion
+    }
+    static int Recursion (int a){
+        int c;
+        if (a == 1){
+            return 1;
+        } else { c = a*Recursion(a-1);
+            return c;
+        }
     }
 }
