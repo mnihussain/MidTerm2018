f4c65e2ad67a9eb6c6022bc434783b5534287833
mnihussain <md.nasiribnehussain@gmail.com>
2018-12-12 12:32:49 -0500

    StringBuffer

--- src/math/problems/StringReverse.java
+++ src/math/problems/StringReverse.java
@@ -2,9 +2,14 @@ package math.problems;
 
 public class StringReverse {
     public static void main(String[] args) {
-        String input = "tutorialspoint";
+        //implementing logic
+        String input = "komola ekhon bonobashe";
         char[] try1 = input.toCharArray();
         for (int i = try1.length-1;i>=0;i--)
             System.out.print(try1[i]);
+
+        // using StringBuffer
+    String str = new StringBuffer(input).reverse().toString();
+        System.out.println(str);
     }
 }
